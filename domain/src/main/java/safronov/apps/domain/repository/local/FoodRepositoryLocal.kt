40047d3a4f1f9ae8a2299a1a2fcec7e45c4ef6c6
safronov <ilya.safa1337w@gmail.com>
2023-11-03 14:35:35 +0500

    Changed FoodRepositoryLocal.kt

--- domain/src/main/java/safronov/apps/domain/repository/local/FoodRepositoryLocal.kt
+++ domain/src/main/java/safronov/apps/domain/repository/local/FoodRepositoryLocal.kt
@@ -1,17 +1,17 @@
 package safronov.apps.domain.repository.local
 
-import safronov.apps.domain.model.food.Food
+import safronov.apps.domain.model.food.FoodItem
 import safronov.apps.domain.model.food_category.FoodCategory
 
 interface FoodRepositoryLocal {
 
     interface GettingFoodRepositoryLocal {
-        suspend fun getAllFoods(): List<Food>
-        suspend fun getFoodsByCategory(category: FoodCategory): List<Food>
+        suspend fun getAllFoods(): List<FoodItem>
+        suspend fun getFoodsByCategory(category: FoodCategory): List<FoodItem>
     }
 
     interface SavingFoodRepositoryLocal {
-        suspend fun saveFoods(list: List<Food>): List<Food>
+        suspend fun saveFoods(list: List<FoodItem>): List<FoodItem>
     }
 
 }
