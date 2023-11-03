a1dd574f841de9c9db3d608422c401cb74206249
safronov <ilya.safa1337w@gmail.com>
2023-11-03 12:03:02 +0500

    Added methods in the FoodRepositoryRemote.kt

--- domain/src/main/java/safronov/apps/domain/repository/remote/FoodRepositoryRemote.kt
+++ domain/src/main/java/safronov/apps/domain/repository/remote/FoodRepositoryRemote.kt
@@ -1,4 +1,11 @@
 package safronov.apps.domain.repository.remote
 
+import safronov.apps.domain.model.food.Food
+import safronov.apps.domain.model.food_category.FoodCategory
+
 interface FoodRepositoryRemote {
+
+    suspend fun getAllFoods(): List<Food>
+    suspend fun getFoodsByCategory(category: FoodCategory): List<Food>
+
 }
