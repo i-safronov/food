8d16b12263ed48f761da35994e4a6d562dd06a95
safronov <ilya.safa1337w@gmail.com>
2023-11-04 10:35:56 +0500

    Added FoodItemEntity.kt

---
+++ data/src/main/java/safronov/apps/data/data_source/local/model/FoodItemEntity.kt
@@ -0,0 +1,60 @@
+package safronov.apps.data.data_source.local.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import safronov.apps.domain.model.food.FoodItem
+
+@Entity(tableName = FoodItemEntity.FOOD_ITEM_ENTITY_TABLE_NAME)
+data class FoodItemEntity(
+    @PrimaryKey(autoGenerate = false) val idMeal: Long? = null,
+    @ColumnInfo val strMeal: String?,
+    @ColumnInfo val strMealThumb: String?
+) {
+
+    companion object {
+        const val FOOD_ITEM_ENTITY_TABLE_NAME = "FoodItemEntityTableName"
+
+        fun convertFoodItemEntityToFoodItem(
+            foodItem: FoodItemEntity
+        ): FoodItem {
+            return FoodItem(
+                idMeal = foodItem.idMeal.toString(),
+                strMeal = foodItem.strMeal,
+                strMealThumb = foodItem.strMealThumb
+            )
+        }
+
+        fun convertFoodItemToFoodItemEntity(
+            foodItem: FoodItem
+        ): FoodItemEntity {
+            return FoodItemEntity(
+                idMeal = foodItem.idMeal?.toLong(),
+                strMeal = foodItem.strMeal,
+                strMealThumb = foodItem.strMealThumb
+            )
+        }
+
+        fun convertListOfFoodItemEntityToListOfFoodItem(
+            list: List<FoodItemEntity>
+        ): List<FoodItem> {
+            val mList = mutableListOf<FoodItem>()
+            list.forEach {
+                mList.add(convertFoodItemEntityToFoodItem(it))
+            }
+            return mList
+        }
+
+        fun convertListOfFoodItemToListOfFoodItemEntity(
+            list: List<FoodItem>
+        ): List<FoodItemEntity> {
+            val mList = mutableListOf<FoodItemEntity>()
+            list.forEach {
+                mList.add(convertFoodItemToFoodItemEntity(it))
+            }
+            return mList
+        }
+
+    }
+
+}
