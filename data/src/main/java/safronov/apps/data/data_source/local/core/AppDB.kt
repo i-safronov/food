2bfa4846724bafabed5212923a8e8e0916f2af9a
safronov <ilya.safa1337w@gmail.com>
2023-11-04 10:56:10 +0500

    Added AppDB.kt

--- data/src/main/java/safronov/apps/data/data_source/local/core/AppDB.kt
+++ data/src/main/java/safronov/apps/data/data_source/local/core/AppDB.kt
@@ -1,7 +1,15 @@
 package safronov.apps.data.data_source.local.core
 
+import androidx.room.Database
+import safronov.apps.data.data_source.local.core.dao.food.FoodDao
+import safronov.apps.data.data_source.local.core.dao.food_category.FoodCategoryDao
+import safronov.apps.data.data_source.local.model.FoodCategoryItemEntity
+import safronov.apps.data.data_source.local.model.FoodItemEntity
+
+@Database(entities = [FoodCategoryItemEntity::class, FoodItemEntity::class], version = 0)
 abstract class AppDB {
 
-
+    abstract fun getFoodDao(): FoodDao
+    abstract fun getFoodCategoryDao(): FoodCategoryDao
 
 }
