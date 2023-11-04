bd50e37b72efebeb2fc9e0f6500cae136cd90ab7
safronov <ilya.safa1337w@gmail.com>
2023-11-04 14:30:29 +0500

    Added App.kt

---
+++ app/src/main/java/safronov/apps/food/app/App.kt
@@ -0,0 +1,17 @@
+package safronov.apps.food.app
+
+import android.app.Application
+import safronov.apps.food.di.component.AppComponent
+import safronov.apps.food.di.component.DaggerAppComponent
+import safronov.apps.food.di.module.AppModule
+
+class App: Application() {
+
+    private var appComponent: AppComponent? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerAppComponent.builder().appModule(AppModule(context = this)).build()
+    }
+
+}
--- app/src/main/java/safronov/apps/food/di/component/AppComponent.kt
+++ app/src/main/java/safronov/apps/food/di/component/AppComponent.kt
@@ -1,9 +1,12 @@
 package safronov.apps.food.di.component
 
 import dagger.Component
+import safronov.apps.food.di.module.AppModule
+import safronov.apps.food.di.module.DataModule
+import safronov.apps.food.di.module.DomainModule
 import safronov.apps.food.ui.fragment.main.main_content.menu.FragmentMenu
 
-@Component
+@Component(modules = [AppModule::class, DomainModule::class, DataModule::class])
 interface AppComponent {
 
     fun inject(fragmentMenu: FragmentMenu)
