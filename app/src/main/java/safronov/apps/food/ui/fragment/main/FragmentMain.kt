c13f20c3d9806fd43986a40aa1fd051dca5c11f9
safronov <ilya.safa1337w@gmail.com>
2023-11-04 18:28:24 +0500

    Changed FragmentMain.kt

--- app/src/main/java/safronov/apps/food/ui/fragment/main/FragmentMain.kt
+++ app/src/main/java/safronov/apps/food/ui/fragment/main/FragmentMain.kt
@@ -5,58 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import safronov.apps.food.R
+import safronov.apps.food.databinding.FragmentMainBinding
+import safronov.apps.food.ui.base.FragmentBase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class FragmentMain : FragmentBase() {
 
-//TODO унаследуйся от FragmentBase
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentMain.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FragmentMain : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun createUi(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun setup() {
+        super.setup()
+        val navHost = childFragmentManager.findFragmentById(R.id.main_content_graph_container) as NavHostFragment?
+        navHost?.navController?.let {
+            binding.mainContentBottomNav.setupWithNavController(it)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    override fun removeUi() {
+        _binding = null
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentMain.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentMain().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = FragmentMain()
     }
+
 }
