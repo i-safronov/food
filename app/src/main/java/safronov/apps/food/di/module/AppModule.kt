c1bdade1e0e0adf0c936f2ac3bbdf783a2644a5c
safronov <ilya.safa1337w@gmail.com>
2023-11-04 14:21:38 +0500

    Added providers in the AppModule.kt and added FragmentMenuViewModelFactory.kt

---
+++ app/src/main/java/safronov/apps/food/di/module/AppModule.kt
@@ -0,0 +1,39 @@
+package safronov.apps.food.di.module
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import safronov.apps.domain.use_case.local.category.GetFoodCategoriesLocalUseCase
+import safronov.apps.domain.use_case.local.food.GetFoodsByCategoryLocalUseCase
+import safronov.apps.domain.use_case.remote.category.GetFoodCategoriesRemoteUseCase
+import safronov.apps.domain.use_case.remote.food.GetFoodsByCategoryRemoteUseCase
+import safronov.apps.food.ui.base.coroutines.DispatchersList
+import safronov.apps.food.ui.fragment.main.main_content.menu.view_model.FragmentMenuViewModelFactory
+import safronov.apps.food.ui.system.network.ConnectivityObserver
+
+@Module
+class AppModule(
+     private val context: Context
+) {
+
+    @Provides
+    fun provideContext(): Context {
+        return context
+    }
+
+    @Provides
+    fun provideFragmentMenuViewModelFactory(
+         dispatchersList: DispatchersList,
+         connectivityObserver: ConnectivityObserver,
+         getFoodCategoriesRemoteUseCase: GetFoodCategoriesRemoteUseCase,
+         getFoodsByCategoryRemoteUseCase: GetFoodsByCategoryRemoteUseCase,
+         getFoodCategoriesLocalUseCase: GetFoodCategoriesLocalUseCase,
+         getFoodsByCategoryLocalUseCase: GetFoodsByCategoryLocalUseCase
+    ): FragmentMenuViewModelFactory {
+        return FragmentMenuViewModelFactory(
+            dispatchersList, connectivityObserver, getFoodCategoriesRemoteUseCase,
+            getFoodsByCategoryRemoteUseCase, getFoodCategoriesLocalUseCase, getFoodsByCategoryLocalUseCase
+        )
+    }
+
+}
---
+++ app/src/main/java/safronov/apps/food/ui/fragment/main/main_content/menu/view_model/FragmentMenuViewModelFactory.kt
@@ -0,0 +1,32 @@
+package safronov.apps.food.ui.fragment.main.main_content.menu.view_model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import safronov.apps.domain.use_case.local.category.GetFoodCategoriesLocalUseCase
+import safronov.apps.domain.use_case.local.food.GetFoodsByCategoryLocalUseCase
+import safronov.apps.domain.use_case.remote.category.GetFoodCategoriesRemoteUseCase
+import safronov.apps.domain.use_case.remote.food.GetFoodsByCategoryRemoteUseCase
+import safronov.apps.food.ui.base.coroutines.DispatchersList
+import safronov.apps.food.ui.system.network.ConnectivityObserver
+
+class FragmentMenuViewModelFactory(
+    private val dispatchersList: DispatchersList,
+    private val connectivityObserver: ConnectivityObserver,
+    private val getFoodCategoriesRemoteUseCase: GetFoodCategoriesRemoteUseCase,
+    private val getFoodsByCategoryRemoteUseCase: GetFoodsByCategoryRemoteUseCase,
+    private val getFoodCategoriesLocalUseCase: GetFoodCategoriesLocalUseCase,
+    private val getFoodsByCategoryLocalUseCase: GetFoodsByCategoryLocalUseCase
+): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return FragmentMenuViewModel(
+            dispatchersList,
+            connectivityObserver,
+            getFoodCategoriesRemoteUseCase,
+            getFoodsByCategoryRemoteUseCase,
+            getFoodCategoriesLocalUseCase,
+            getFoodsByCategoryLocalUseCase
+        ) as T
+    }
+
+}
